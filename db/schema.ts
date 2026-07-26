6d81dd5e2259b86f7cab6777fa44d0e522ff67db
MeTOR1657 <chokun246810@gmail.com>
2026-07-26 13:38:11 +0700

    Initial commit

---
+++ db/schema.ts
@@ -0,0 +1,29 @@
+import {
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+  boolean,
+} from "drizzle-orm/pg-core";
+
+export const todoTable = pgTable("todo", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  todoText: varchar("todo_text", { length: 255 }).notNull(),
+  deadline: timestamp("deadline", { mode: "string" }),
+  isDone: boolean("is_done").default(false),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at", { mode: "date", precision: 3 }).$onUpdate(
+    () => new Date(),
+  ),
+});
+
+export const accountTable = pgTable("account", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  username: varchar("username", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at", { mode: "date", precision: 3 }).$onUpdate(
+    () => new Date(),
+  ),
+});
